1aaaadc239d1f42eaf29b505a2fe533baaed147d
DaNyrisCode <komakhinamarina@gmail.com>
2025-03-26 16:45:18 +0100

    ortho2 -_-

--- src/pages/Competences.jsx
+++ src/pages/Competences.jsx
@@ -35,7 +35,7 @@ function Competences() {
 
 			{/* Hard Skills */}
 			<div className="hard-skills-container">
-				<h3>J'ai travailler avec</h3>
+				<h3>J'ai travaillé avec</h3>
 				<motion.div
 					className="tech-skills"
 					initial={{ opacity: 0 }}
